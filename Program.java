8bb469e37820dce0767dd6b4c74fd5ca4802242c
HridayNayak25 <hridaynayak8@gmail.com>
2024-09-06 18:07:22 -0700

    Update Program.java
    Program 7 final final code modulo debugged verison.

--- Program.java
+++ Program.java
@@ -1,9 +1,59 @@
-//import stuff here
+public class BuckCoverterModulusupdated {
+      private double outputA;
+      private double outputB;
+      private double outputC;
 
-//Your code here
+    public double convert(double nickel, double klevin, double buck) {
+        double conversion = 0.0;
 
-//Paste console output below:
-/*
+        // Nickels conversion
+        if (nickel == 240.0) {
+            double newKlevin = klevin + 1;
+            nickel = 0;
+            outputA = newKlevin; 
+        } else if (nickel < 240.0) {
+            double newNickel = nickel / 240.0;
+            outputA = newNickel;
+            nickel = 0;
+        } else if (nickel > 240.0) {
+            double newKlevin = (nickel / 240.0) + klevin;
+            outputA = newKlevin;
+          
+            nickel = nickel % 240.0;  // thank you for the % idea sir!
+        }
+
+   
+        if (klevin == 20.0) {
+            double newBuck = buck + 1;
+            klevin = 0;
+            outputB = newBuck;
+        } else if (klevin < 20.0) {
+            double newKlevin = klevin / 20.0;
+          
+            outputB = newKlevin;
+            klevin = 0;
+        } else if (klevin > 20.0) {
+            double newBuck = (klevin / 20.0) + buck;
+            outputB = newBuck;
+            klevin = klevin % 20.0;  
+        }
+
+      
+        if (buck == 1) {
+            outputC = buck;
+        } else if (buck < 1) {
+            outputC = buck;
+        } else if (buck > 1) {
+            outputC = buck;
+        }
+
+        
+        conversion = outputC + outputB + outputA;
+      
+        return conversion;
+    }
+
+    
+}
 
 
-*/
